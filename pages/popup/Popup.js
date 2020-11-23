4577b524fc5c97763b43f84c569b66489990712d
Rocky Gonzales <gonzrock12@gmail.com>
2020-11-23 16:05:03 +0800

    adding chat options based on figma designs

--- pages/popup/Popup.js
+++ pages/popup/Popup.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(e) {
 
+	/**
+	 * Toggle Popup Widget
+	 */
 	const showBtn = document.getElementById('popup-interface-btn'),
 
 		  chatContainer = document.getElementById('chat-widget-interface'),
@@ -14,6 +17,42 @@ document.addEventListener('DOMContentLoaded', function(e) {
 	close.addEventListener('click', () => {
 		chatContainer.classList.remove('show');
 		showBtn.parentElement.classList.remove('hide');
+	});
+
+	/**
+	 * Toggle Chat Options
+	 * toggle between hiding and showing the chat options dropdown content
+	 */
+	const toggleChatOptions = document.getElementById('toggleChatOptions'),
+	 	chatOptions = document.getElementById('chatOptions');
+
+	toggleChatOptions.addEventListener('click', function(e) {
+		e.stopPropagation();
+		chatOptions.classList.toggle("show");
+	});
+
+	/**
+	 * Close the dropdown if the user clicks outside of it
+	 */
+	window.onclick = function(event) {
+		if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("chat-dropdown-options");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+			}
+		}
+		}
+	}
+
+	/**
+	 * toggle Mute/Unmute Chat Options
+	 */
+	const toggleMute = document.getElementById('toggleMute');
+	toggleMute.addEventListener('click', () => {
+		toggleMute.innerHTML = toggleMute.innerHTML === 'Mute' ? 'Unmute' : 'Mute';
 	});
 
 });
